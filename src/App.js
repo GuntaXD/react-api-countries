e04d818daf7000fc06f4e8f7f7dccd3711b5b8d9
GuntaXD <juanferreira65@gmail.com>
2022-03-20 16:32:17 -0400

    cambie el titulo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          this is my app! :V
+          esta es mi app :V
         </p>
         <a
           className="App-link"
